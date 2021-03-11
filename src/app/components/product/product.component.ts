4a67283c71c91ff974bb44d5c3a8b1ff02a7d7cf
fatih eren <fatihhernn@gmail.com>
2021-03-12 00:07:23 +0300

    2.ders sonu

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
-import { HttpClient } from '@angular/common/http';
+import { ProductService } from 'src/app/services/product.service';
+
 
 @Component({
   selector: 'app-product',
@@ -12,21 +12,23 @@ export class ProductComponent implements OnInit {
   //ngOnInit : uygulama ilk açıldığında çalışan ilk uygulamamız
 
   products: Product[] = [];
-  apiUrl = 'https://localhost:44368/api/products/getall';
+  dataLoaded=false;
 
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private productService:ProductService) {}
 
   ngOnInit(): void {
 
     this.getProducts()
-    
+
   }
 
-  getProducts() {
-    this.httpClient.get<ProductResponseModel>(this.apiUrl)
-    .subscribe(((response)=>{
+  getProducts(){
+    this.productService.getProducts().subscribe(response=>{
       this.products=response.data
-    }));
+      this.dataLoaded=true;
+    })
   }
+
+ 
 }
