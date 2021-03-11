985cbf6f0641defac8c5787e0b4198d33f3e1d44
fatih eren <fatihhernn@gmail.com>
2021-03-11 21:14:41 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//component, html datasını yönettiğimiz yer
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind'
+  user: string = "Fatih eren"
+ 
 }
