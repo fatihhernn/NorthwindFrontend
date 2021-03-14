222af5bc6148af4d6a1e121576a719a2230d106c
fatih eren <fatihhernn@gmail.com>
2021-03-14 13:41:23 +0300

    İmprovemented all products button

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -5,39 +5,46 @@ import { CategoryService } from 'src/app/services/category.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-
-
-  categories:Category[]=[];
-  
+  categories: Category[] = [];
   //fake data ile initialize ederiz
-  currentCategory:Category;
+  currentCategory: Category={categoryId:0, categoryName:"Tüm Ürünleri"};
 
-  constructor(private categoryService:CategoryService) { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
-    this.getCategories()
+    this.getCategories();
   }
 
-  getCategories(){
-    this.categoryService.getCategories().subscribe(response=>{
-      this.categories=response.data
-    })
+  getCategories() {
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+    });
   }
 
-  setCurrentCategory(category:Category){
-    this.currentCategory=category
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
   }
 
-  getCurrentCategoryClass(category:Category){
-    if (category==this.currentCategory) {
-      return "list-group-item active"
+  getCurrentCategoryClass(category: Category) {
+    if (category == this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
     }
-    else{
-      return "list-group-item"
+  }
+
+  getAllCategoryClass() {
+    if (this.currentCategory.categoryId==0) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
     }
   }
 
+  setAllCategory(){
+    return this.currentCategory={categoryId:0, categoryName:"Tüm Ürünleri"};
+  }
 }
